a41d7cf03cc5d889e1f0f079069f261819ed512e
charles-richardson8706 <FiremanFFiredog@aol.com>
2021-03-02 06:52:27 -0500

    New Projects

---
+++ Week 1/challenges/Jeopardy/MyJeopardy/jeopardy.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+    retrieveQuestion();
+    document.getElementById("reveal").onclick = reveal;
+    document.getElementById("next").onclick = retrieveQuestion;
+};
+
+function retrieveQuestion() {
+    document.getElementById("qdiv").style.display = 'none';
+    var c = document.getElementById("category");
+    var q = document.getElementById("question");
+    var a = document.getElementById("answer");
+    c.innerHTML = "Loading";
+
+    a.innerHTML = "Loading";
+    fetch('http://jservice.io/api/random')
+    .then((response) => response.json())
+    .then((data) => {
+
+
+
+        c.innerHTML = data[0].category.title;
+        q.innerHTML = data[0].answer;
+        a.innerHTML = data[0].question;
+
+    })
+}
+
+function reveal() {
+    document.getElementById("qdiv").style.display = 'block';
+}
